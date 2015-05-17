08aa96a118117e224423faa403b1156035f95f87
Jochen Kupperschmidt <homework@nwsnet.de>
2015-05-17 14:33:01 +0200

    Extracted functions.

--- cli.py
+++ cli.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-"""cli.py -- Version 04-Feb-2006
+"""cli.py -- Version 17-May-2015
 
 A command line interface to the Mnemonic Password Generator.
 
@@ -18,33 +18,69 @@ from optparse import OptionParser
 import mnemonicpasswords
 
 
-if __name__ == '__main__':
-    # Create option parser and define options.
+def create_parser():
     parser = OptionParser(
         usage='%prog [options]',
-        description='Generate pronounceable and thereby easier remembered passwords.')
-    parser.add_option('-l', '--letters',
-        type='int', dest='letters', default=6,
-        help='number of letters (default: 6)', metavar='NUM')
-    parser.add_option('-d', '--digits',
-        type='int', dest='digits', default=2,
-        help='number of digits (default: 2)', metavar='NUM')
-    parser.add_option('-u', '--uppercase',
-        action='store_true', dest='uppercase', default=False,
-        help='uppercase letters')
-    parser.add_option('-p', '--passwords',
-        type='int', dest='passwords', default=1,
-        help='number of passwords to generate (default: 1)', metavar='NUM')
+        description='Generate pronounceable and thereby easier '
+                    'remembered passwords.')
 
-    # Process options and arguments.
-    (options, args) = parser.parse_args()
+    parser.add_option('-l', '--letters',
+        type='int',
+        dest='letters',
+        default=6,
+        help='number of letters (default: 6)',
+        metavar='NUM')
+
+    parser.add_option('-d', '--digits',
+        type='int',
+        dest='digits',
+        default=2,
+        help='number of digits (default: 2)',
+        metavar='NUM')
+
+    parser.add_option('-u', '--uppercase',
+        action='store_true',
+        dest='uppercase',
+        default=False,
+        help='uppercase letters')
+
+    parser.add_option('-p', '--passwords',
+        type='int',
+        dest='passwords',
+        default=1,
+        help='number of passwords to generate (default: 1)',
+        metavar='NUM')
+
+    return parser
+
+
+def parse_options():
+    parser = create_parser()
+
+    options, args = parser.parse_args()
     if len(args):
         parser.print_help()
-        parser.error('No arguments allowed, options only.')
+        parser.error('No arguments allowed; options only.')
 
-    # Print passwords as requested.
+    return options
+
+
+def generate_passwords(total, letters, digits, uppercase):
     for i in range(options.passwords):
-        print mnemonicpasswords.generateMnemonicPassword(
+        yield mnemonicpasswords.generateMnemonicPassword(
             letters=options.letters,
             digits=options.digits,
             uppercase=options.uppercase)
+
+
+if __name__ == '__main__':
+    options = parse_options()
+
+    passwords = generate_passwords(
+        options.passwords,
+        options.letters,
+        options.digits,
+        options.uppercase)
+
+    for password in passwords:
+        print password
