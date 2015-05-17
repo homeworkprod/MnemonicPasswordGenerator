97639e53ab633e458b863c075988b03290ecb08f
Jochen Kupperschmidt <homework@nwsnet.de>
2015-05-17 16:15:42 +0200

    Specify encoding in each source code file.

--- cli.py
+++ cli.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
 A command line interface to the Mnemonic Password Generator.
--- tkintergui.py
+++ tkintergui.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
 A graphical frontend to the Mnemonic Password Generator in Tkinter.
